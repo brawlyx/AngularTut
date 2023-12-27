9614034181522ff3b026973dc58c09cd77cc3e92
StevenHelliwell <stevehelliwell12@gmail.com>
2023-12-27 11:05:59 +0000

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    
+    RecipesComponent,
+    RecipeListComponent,
   ],
   imports: [
     BrowserModule,
