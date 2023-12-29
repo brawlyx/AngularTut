1638a5268e084470c5082a710f0b9a758303ed6b
StevenHelliwell <stevehelliwell12@gmail.com>
2023-12-29 16:02:55 +0000

    setup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+ import { Component,OnInit} from '@angular/core';
+import { Recipe } from '../recipe.model';
+ @Component({
+   selector: 'app-recipe-list',
+   templateUrl: './recipe-list.component.html',
+   styleUrls: ['./recipe-list.component.css']
+ })
+ export class RecipeListComponent implements OnInit {
+   recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWI8Erz_akCEuEzV24i8OdYyuK9a6geXKReA&usqp=CAU')
+   ];
 
-@Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
-})
-export class RecipeListComponent {
-
-}
+   constructor(){}
+   ngOnInit(): void {
+     throw new Error('Method not implemented.');
+   }
+  
+ }
